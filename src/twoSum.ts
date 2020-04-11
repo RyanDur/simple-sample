b3c466896d7ef9f8d81d42847c6c828bfbaafae7
Ryan Durling <ryan.p.durling@gmail.com>
2020-04-11 17:26:48 -0500

    a solution that performs better

--- src/twoSum.ts
+++ src/twoSum.ts
@@ -1,18 +1,15 @@
-const range = (size: number) => {
-  return [...Array(size).keys()];
-};
-
 export const twoSum = (nums: number[] = [], target: number): number[] => {
-  const hash = new Map();
-  return nums.reduce((acc, num, index) => {
-    const compliment = target - num;
-    const values = Array.from(hash.values());
-    const complimentIndex = values.indexOf(compliment);
-    if (values.includes(compliment) && complimentIndex != index) {
-      return [values.indexOf(compliment), index];
-    } else {
-      hash.set(index, num);
-      return acc;
-    };
-  }, []);
+  const numsIndexes = nums.reduce((acc, num, index) => {
+    acc[num] = index;
+    return acc;
+  }, {});
+
+  for (let i = 0; i < nums.length; i += 1) {
+    const compliment = target - nums[i];
+    const complimentIndex = numsIndexes[compliment];
+    if (complimentIndex && complimentIndex !== i) {
+      return [i, complimentIndex];
+    }
+  }
+  return [];
 };
