59ffcafbf3004c8a20b816a5764ca49d35b0c8b0
Ryan Durling <ryan.p.durling@gmail.com>
2020-09-06 14:25:53 -0500

    asdf

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,35 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const {configCSS, loadImages, loadTS, merge} = require("@ryandur/webpack-configs")
+/* eslint-disable @typescript-eslint/no-var-requires */
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {
+  configCSS,
+  loadImages,
+  loadTS,
+  merge
+} = require('@ryandur/webpack-configs');
+
 module.exports = merge(
   loadTS(),
-  configCSS({sourceMap: true, production: true}),
+  configCSS({ sourceMap: true, production: true }),
   loadImages(),
   {
-    devtool: "source-map",
-    mode: "production",
+    devtool: 'source-map',
+    mode: 'production',
     resolve: {
-      extensions: [".ts", ".js"]
+      extensions: ['.ts', '.js']
     },
-    plugins: [new HtmlWebpackPlugin({
-      title: "Webpack demo",
-      template: "src/index.html",
-      meta: {
-        viewport: "width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
-      }
-    })]
-  })
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Webpack demo',
+        template: 'src/index.html',
+        meta: {
+          viewport:
+            'width=device-width, ' +
+            'user-scalable=no, ' +
+            'initial-scale=1.0, ' +
+            'maximum-scale=1.0, ' +
+            'minimum-scale=1.0'
+        }
+      })
+    ]
+  }
+);
