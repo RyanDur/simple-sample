a03bd4c83fdbeea62d6e51f5e9a8b171d518a354
Ryan Durling <ryan.p.durling@gmail.com>
2020-09-06 19:29:33 -0500

    explain meta viewport for responsive design

--- webpack.config.js
+++ webpack.config.js
@@ -22,12 +22,20 @@ module.exports = merge(
         title: 'Webpack demo',
         template: 'src/index.html',
         meta: {
-          viewport:
-            'width=device-width, ' +
-            'user-scalable=no, ' +
-            'initial-scale=1.0, ' +
-            'maximum-scale=1.0, ' +
-            'minimum-scale=1.0'
+          viewport: [
+            'width=device-width', // sets the width of the page to follow the
+            // screen-width of the device
+            'initial-scale=1.0' // sets the initial zoom level when the page
+            // is first loaded by the browser.
+
+            // BELOW ARE NOT RECOMMENDED BECAUSE IT TAKES THE CONTROL AWAY FROM
+            //                            THE USER
+            // 'user-scalable=no,', // Allows the device to zoom in and out.
+            // 'maximum-scale=1.0,', // The minimum amount the visitor can zoom
+            // on the page. 1.0 does not zoom.
+            // 'minimum-scale=1.0' // The maximum amount the visitor can zoom on
+            // the page. 1.0 does not zoom.
+          ].join(',')
         }
       })
     ]
